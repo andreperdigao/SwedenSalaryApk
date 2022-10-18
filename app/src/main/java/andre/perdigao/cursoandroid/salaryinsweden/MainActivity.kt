c3cf43c788aa42e838bd3956675587e7ff30c38a
andreperdigao <andre_perdigao@hotmail.com>
2022-10-18 20:35:21 +0200

    segundo commit

--- app/src/main/java/andre/perdigao/cursoandroid/salaryinsweden/MainActivity.kt
+++ app/src/main/java/andre/perdigao/cursoandroid/salaryinsweden/MainActivity.kt
@@ -1,8 +1,11 @@
 package andre.perdigao.cursoandroid.salaryinsweden
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -81,9 +84,12 @@ class MainActivity : AppCompatActivity() {
             txtLifeCostValue.setText(Cost.toString())
             txtRestValue.setText(Rest.toString())
 
+            closeKeyboard(txtRestValue)
+
 
             Toast.makeText(this,"Calculating...",
             Toast.LENGTH_SHORT).show()
+
         }
 
         btnClear.setOnClickListener {
@@ -107,6 +113,15 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun closeKeyboard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+        }
+
+    }
+
+
     /***
 
     fun calculateEvent(view: View) {
